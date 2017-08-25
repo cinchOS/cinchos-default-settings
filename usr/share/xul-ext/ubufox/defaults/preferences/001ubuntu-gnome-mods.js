6e4db92b021c3f51a77e21cba8f1ab400d88821e
Unknown <tohur@gamerslair.net>
2017-08-24 22:51:04 -0600

    first commit

---
+++ usr/share/xul-ext/ubufox/defaults/preferences/001ubuntu-gnome-mods.js
@@ -0,0 +1,6 @@
+// Place your global Firefox preferences in this file if you are using
+// ubufox. Especially those preferences defined in ubufox need to be configured
+// here to become effective.
+
+// Example: Homepage
+pref("browser.startup.homepage", "file:/usr/share/xul-ext/ubufox/defaults/preferences/ubuntu-gnome-homepage.properties");
